da6881f674adc0c729f269ae6fb7b5df219f63cc
sina03030 <sinahosein1382@gmail.com>
2022-04-02 17:21:27 +0430

    config.env varuable added to project

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const path = require('path');
 
 const express = require('express');
+const dotEnv = require('dotenv');
 
 const indexRoutes = require('./routes');
+
+// setting config.env files to system environemt variables
+dotEnv.config({path: "./config/config.env"});
 
 const app = express();
 
 //* view engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 //* statics
 app.use(express.static(path.join(__dirname, 'public')));
 
 //* routes
 app.use(indexRoutes)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=> console.log(`server is up and runnig on port ${PORT}`));
