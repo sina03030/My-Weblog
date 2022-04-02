e3b6ba55b5d9335a8e6cbd3a15dc1ca77a786cab
sina03030 <sinahosein1382@gmail.com>
2022-04-02 18:25:02 +0430

    morgan added for logging

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const path = require('path');
 
 const express = require('express');
 const dotEnv = require('dotenv');
+const morgan = require('morgan');
 
 const indexRoutes = require('./routes');
 
 // setting config.env files to system environemt variables
-dotEnv.config({path: "./config/config.env"});
+dotEnv.config({ path: "./config/config.env" });
 
 const app = express();
+
+//* custom middleware
+// logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 
 //* view engine
 app.set('view engine', 'ejs');
@@ -18,7 +26,7 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //* routes
-app.use(indexRoutes)
+app.use(indexRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=> console.log(`server is up and runnig on port ${PORT}`));
+app.listen(PORT, () => console.log(`server is up and runnig in ${process.env.NODE_ENV} on port ${PORT}`));
