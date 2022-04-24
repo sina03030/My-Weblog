49b573b8ec88c4a48300d86b33b3c97d8e2aa0a1
sina03030 <sinahosein1382@gmail.com>
2022-04-24 13:03:49 +0430

    fit: Add upload image controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,14 @@
+const multer = require('multer');
+const uuid = require('uuid').v4;
+const sharp = require('sharp');
+
 const Blog = require('../models/Blog');
 const dateFormat = require('../utils/date_format');
-const {get500} = require('../controllers/errorController');
+const { get500 } = require('../controllers/errorController');
+const { storage, fileFilter } = require('../utils/multer');
 
 exports.getDashboard = async (req, res) => {
-    const blogs = await Blog.find({user: req.user.id});
+    const blogs = await Blog.find({ user: req.user.id });
     try {
         res.render('private/blogs', {
             pageTitle: 'Dashboard',
@@ -52,4 +57,37 @@ exports.createPost = async (req, res) => {
             errors: errArr,
         });
     }
+}
+
+exports.uploadImage = (req, res) => {
+
+    const upload = multer({
+        // storage: storage,
+        // dest: 'uploads/',
+        limits: { fileSize: 4000000 },
+        fileFilter: fileFilter,
+    }).single('image');
+
+    upload(req, res, async (err) => {
+        if (err) {
+            if (err.code === 'LIMIT_FILE_SIZE') {
+                return res.send('file size is too large');
+            }
+            res.send(err);
+        } else {
+
+            if (req.file) {
+                console.log(req.file);
+                const fileName = `${uuid()}_${req.file.originalname}`;
+
+                await sharp(req.file.buffer)
+                    .jpeg({ quality: 50 }).toFile(`./public/uploads/${fileName}`)
+                    .catch(err => console.log(err));
+
+                res.status(200).send(`http://localhost:3000/uploads/${fileName}`);
+            } else {
+                res.send('Please select an image');
+            }
+        }
+    });
 }
